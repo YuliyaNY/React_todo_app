a90a8d0e0cb5d5654e1497a38cb2aa18fc53cda3
Yuliya <yuliyababarykina@gmail.com>
2020-01-06 18:12:37 -0500

    Updates

---
+++ precache-manifest.778baf4d1ad3976fa8bcd6d2367d94b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4533e4e0e4ec3dc23e6ae59ac32876f2",
+    "url": "/React_todo_app/index.html"
+  },
+  {
+    "revision": "f0ca5d2f2a11ea442888",
+    "url": "/React_todo_app/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "c10aa5cd6da9091dc84e",
+    "url": "/React_todo_app/static/js/2.701d0d9a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/React_todo_app/static/js/2.701d0d9a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0ca5d2f2a11ea442888",
+    "url": "/React_todo_app/static/js/main.e9193252.chunk.js"
+  },
+  {
+    "revision": "89a4ecade51ffa35f1a9",
+    "url": "/React_todo_app/static/js/runtime-main.2ca9921e.js"
+  }
+]);
